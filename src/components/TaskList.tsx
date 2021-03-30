c1792a1c9a24764597e9008f964d452644107938
renanlucaz <renan@weef.com.br>
2021-03-30 14:56:34 -0300

    Desafio concluído

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,41 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) {
+      return
+    }
+
+    const task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks(prevState => [...prevState, task]);
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let newTaskToggle = tasks.find(task => task.id === id);
+    
+    if(newTaskToggle){
+      newTaskToggle.isComplete = !newTaskToggle.isComplete
+
+      setTasks(prevState => [...prevState].map(item => {
+        const { id, title, isComplete } = item;
+        return {
+          id,
+          title,
+          isComplete,
+        }
+      }));
+    }
+
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(newTasks);
   }
 
   return (
